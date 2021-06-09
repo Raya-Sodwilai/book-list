7a313170985dc3155da979b1911e400d9d500289
Raya Sodwilai <sodwilai@pdx.edu>
2021-06-09 13:55:12 -0700

    fix typo in views

--- BookList/Models/Book.cs
+++ BookList/Models/Book.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace BookList.Models
 {
   public class Book
   {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public string Author { get; set; }
     public string ISBN { get; set; }
